614c1b3fe30fe01b981081b9b16962fab4097371
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2024-02-11 22:28:34 -0800

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,16 +4,14 @@ import json
 
 
 class Base:
-    '''A representation of the base of our OOP hierarchy'''
+    """A representation of the base of our OOP hierarchy"""
 
     __nb__objects = 0
 
-    def __init(self, id -None):
-        '''Constructor'''
+    def __init(self, id=None):
+        """Constructor"""
         if id is not None:
-        '''give a unique id'''
             self.id = id
         else:
-        '''if no id given give the next id'''
             Base._nb_objects += 1
-            self.id = Base.__nb_objects 
+            self.id = Base.__nb_objects
